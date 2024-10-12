730059b269b6bc60f26ceda3fb69214968339a5c
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2024-10-12 18:04:14 +0100

    update question data

--- src/data.js
+++ src/data.js
@@ -1,27 +1,28 @@
 const questions = [
   {
     id: 1,
-    title: "Do I have to allow the use of cookies?",
+    title: "Who closes the bus door when the driver gets off?",
     info: "Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.",
   },
   {
     id: 2,
-    title: "How do I change my Page password?",
+    title: "If I drop soap on the floor is the floor clean or the soap dirty?",
     info: "Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.",
   },
   {
     id: 3,
-    title: "What is BankID?",
+    title:
+      "If two mind readers read each other's minds, whose mind are they really reading?",
     info: "Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.",
   },
   {
     id: 4,
-    title: "Whose birth date can I use?",
+    title: "If I try to fail, but succeed, which one did I do?",
     info: "Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.",
   },
   {
     id: 5,
-    title: "When do I recieve a password ordered by letter?",
+    title: "If Money doesn't grow on trees, why do banks have branches?",
     info: "Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ",
   },
 ];
